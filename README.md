183264944de030689ee9d85f499111fbd28daf05
shamqmq <eng.mohammed447@gmail.com>
2024-12-28 10:30:46 +0200

    pushing files

---
+++ README.md
@@ -0,0 +1,23 @@
+# dots
+my dotfiles :3
+
+
+
+## Todo list
+- [x] audio fix and cards definition
+- [x] slstatus  and its configs
+- [x] ranger download and configure
+- [x] internet connection
+- [x] mpv config
+- [ ] zsh configure and downloading
+- [x] neovim configs
+- [x] scripts:
+  - [x]    mounting, unmounting and ejection external disks
+  - [x]    screanshot
+  - [x]    openning video in terminal with url
+  - [x]    volume manipulatin
+
+- [x] image editing [my paint]
+- [x] pdf [zathura]
+- [x] fonts
+- [x] fuzzy finder [fzf]
---
+++ needs/utilities.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+# audio definition
+doas xbps-install -Sy alsa-pipewire libjack-pipewire pipewire alsa-utils AlsaMixer.app
+echo "audio definitions have been installed"
+#internet and social media
+
+doas xbps-install -Sy firefox wget telegram-desktop
+
+echo "internet and social media have been installed"
+
+# ricing 
+doas xbps-install -Sy pywal feh picom xrdb xdo xdotool setxkbmap nerd-fonts-symbols-ttf noto-fonts-ttf noto-fonts-ttf-extra
+
+echo "ricing have been installed"
+
+#utilites
+doas xbps-install -Sy sxhkd htop fzf fd thunar zsh ranger ntfs-3g ufw
+doas ufw enable
+
+echo "utilites have been installed"
+#media playing , downloading and manipulating
+ 
+doas xbps-install -Sy mpv sxiv ueberzug yt-dlp ffmpeg atomicparsley gimp mypaint zathura zathura-pdf-mupdf scrot
+echo "media playing , downloading and manipulating have been installed"
+
+
+# internet
+doas xbps-install -Sy NetworkManager
+doas unlink /var/service/wpa_supplicant
+doas ln -s /etc/sv/NetworkManager /var/service
+doas ln -s /etc/sv/dbus /var/service
