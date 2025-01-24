280000e5139e2fd4e574d0040992fb67eabebf06
shamqmq <eng.mohammed447@gmail.com>
2025-01-24 14:03:10 +0200

    adding libs

--- needs/ess.sh
+++ needs/ess.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
 # imporatant 
-sudo xbps-install -S make gcc libcurl-devel xorg-server libX11-devel libXft-devel libXinerama-devel xorg-input-drivers xinit xauth xorg-fonts pkg-config xclip xorg-video-drivers libswresample  
-
+sudo xbps-install -S make gcc libcurl-devel xorg-server libX11-devel libXft-devel libXinerama-devel xorg-input-drivers xinit xauth xorg-fonts pkg-config xclip xorg-video-drivers libswresample harfbuzz-devel libXext-devel libXrender-devel gd-devel imlib2 imlib2-devel
